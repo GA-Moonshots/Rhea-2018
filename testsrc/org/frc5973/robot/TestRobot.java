eee089368193c23bd1a2f58106f4619fadec5fb9
dadiletta <dan.adiletta@gmail.com>
2018-01-11 08:37:55 -0500

    initial commit

---
+++ testsrc/org/frc5973/robot/TestRobot.java
@@ -0,0 +1,13 @@
+/* Created Wed Jan 10 16:54:20 EST 2018 */
+package org.frc5973.robot;
+
+import org.junit.Test;
+
+public class TestRobot {
+
+    @Test
+    public void test() {
+        // do something here
+    }
+
+}
