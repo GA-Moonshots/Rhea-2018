6065e4fb75a15c8f19c790d5aa6af587e31f939d
Sebastian Williams <sebomonz@gmail.com>
2018-01-23 16:37:15 -0500

    added joystick code and commented out the Smart DashBoard stuff because it was causing compile errors

--- src/org/frc5973/robot/Robot.java
+++ src/org/frc5973/robot/Robot.java
@@ -5,49 +5,150 @@
  */
 package org.frc5973.robot;
 
-import org.strongback.Strongback;
-import org.strongback.command.Command;
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
 
+import org.frc5973.robot.*;
+import org.strongback.Strongback;
+import org.strongback.SwitchReactor;
+import org.strongback.command.Command;
+import org.strongback.components.Motor;
+import org.strongback.components.ui.ContinuousRange;
+import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
+import org.strongback.hardware.Hardware;
+import org.strongback.util.Values;
+
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 
-
 public class Robot extends IterativeRobot {
+
+	// Declares the ports for the motors and joysticks
+	private static final int JOYSTICK_PORT = 0; // in driver station
+	private static final int RMOTOR_FRONT = 3;
+	private static final int RMOTOR_REAR = 2;
+	private static final int LMOTOR_FRONT = 0;
+	private static final int LMOTOR_REAR = 1;
+
+	// Declares the ports for the winch and door
+	private static final int WINCH_PORT = 4;
+	private static final int DOOR_PORT = 5;
+	private static final int WINCH2_PORT = 6;
+
+	// Declares the TankDrive reference along with the ContinuousRange objects
+	private TankDrive drive;
+	private ContinuousRange driveSpeed;
+	private ContinuousRange turnSpeed;
+	private ContinuousRange turnSpeed2;
+
+	// We moved this up here so we can output this variable in the teleop
+	protected ContinuousRange sensitivity;
+
+	// Used to limit and format the number of console outputs
+	private int filter = 0;
+	private String pattern = "###.###";
+	private DecimalFormat myFormat = new DecimalFormat(pattern);
+	private double sen;
 
 	Command autonomousCommand;
 	@SuppressWarnings("rawtypes")
 	SendableChooser autoChooser;
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
-    public void robotInit() {
-    	autoChooser = new SendableChooser();
-    	autoChooser.addDefault("Default program", new JustForward());
-    	autoChooser.addObject("Right start, right switch", new JustForward());
-    }
-    
-    public void autonomousInit(){
-        //Start Strongback functions ...
-        Strongback.start();    
-        autonomousCommand = (Command) autoChooser.getSelected();
-        Strongback.submit(autonomousCommand);
-    }
-    
+	public void robotInit() {
+		Strongback.configure().recordNoData().recordNoCommands().recordNoEvents()
+				.useExecutionPeriod(200, TimeUnit.MILLISECONDS).initialize();
+		// Sets up the two cameras, one facing forward and once facing backwards
+		CameraServer.getInstance().startAutomaticCapture(0);
+		CameraServer.getInstance().startAutomaticCapture(1);
+		// Set up the robot hardware ...
+		Motor left_front = Hardware.Motors.victorSP(LMOTOR_FRONT).invert(); // left
+																			// rear
+		Motor left_rear = Hardware.Motors.victorSP(LMOTOR_REAR).invert(); // left
+																			// front
+		// DoubleToDoubleFunction SPEED_LIMITER = Values.limiter(-0.1, 0.1);
+		Motor right_front = Hardware.Motors.victorSP(RMOTOR_FRONT); // right
+																	// rear
+		Motor right_rear = Hardware.Motors.victorSP(RMOTOR_REAR); // right front
 
-    @Override
-    public void teleopInit() {
-        //Start Strongback functions ...
-        Strongback.start();
-    }
+		Motor left = Motor.compose(left_front, left_rear);
+		Motor right = Motor.compose(right_front, right_rear);
 
-    @Override
-    public void teleopPeriodic() {
-    }
+		Motor winch = Hardware.Motors.victorSP(WINCH_PORT);
+		Motor winch2 = Hardware.Motors.victorSP(WINCH2_PORT);
 
-    @Override
-    public void disabledInit() {
-        // Tell Strongback that the robot is disabled so it can flush and kill commands.
-        Strongback.disable();
-    }
+		Motor winch_compose = Motor.compose(winch, winch2);
+		Motor door = Hardware.Motors.victorSP(DOOR_PORT);
+
+		drive = new TankDrive(left, right);
+		// Set up the human input controls for teleoperated mode. We want to use
+		// the Logitech Attack 3D's throttle as a
+		// "sensitivity" input to scale the drive speed and throttle, so we'll
+		// map it from it's native [-1,1] to a simple scale
+		// factor of [0,1] ...
+		FlightStick joystick = Hardware.HumanInterfaceDevices.logitechExtreme3D(JOYSTICK_PORT);
+
+		SwitchReactor reactor = Strongback.switchReactor();
+		ContinuousRange sensitivity = joystick.getThrottle().map(t -> ((t + 1.0) / 2.0));
+		sensitivity = joystick.getThrottle().map(Values.mapRange(-1.0, 1.0).toRange(0.0, 1.0));
+		driveSpeed = joystick.getPitch().scale(sensitivity::read).invert(); // scaled
+		turnSpeed = joystick.getRoll().scale(sensitivity::read);
+		turnSpeed2 = joystick.getYaw().scale(sensitivity::read);
+		// scaled and
+		// inverted
+		reactor.onTriggered(joystick.getButton(7), () -> switchControls());
+
+		reactor.onTriggered(joystick.getButton(3), () -> winch_compose.setSpeed(1));
+		reactor.onUntriggered(joystick.getButton(3), () -> winch_compose.stop());
+		// reactor.onTriggered(joystick.getButton(4), () -> winch.setSpeed(-1));
+		// reactor.onUntriggered(joystick.getButton(4), () -> winch.stop());
+
+		reactor.onTriggered(joystick.getButton(12), () -> door.setSpeed(1));
+		reactor.onUntriggered(joystick.getButton(12), () -> door.stop());
+
+		reactor.onTriggered(joystick.getButton(11), () -> door.setSpeed(-1));
+		reactor.onUntriggered(joystick.getButton(11), () -> door.stop());
+		/*
+		autoChooser = new SendableChooser();
+		autoChooser.addDefault("Default program", new JustForward());
+		autoChooser.addObject("Right start, right switch", new JustForward());
+		*/
+	}
+
+	public void autonomousInit() {
+		// Start Strongback functions ...
+		Strongback.start();
+		autonomousCommand = (Command) autoChooser.getSelected();
+		Strongback.submit(autonomousCommand);
+	}
+
+	@Override
+	public void teleopInit() {
+		// Kill anything running if it is ...
+		Strongback.disable();
+		// Start Strongback functions ...
+		Strongback.start();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		drive.arcade(driveSpeed.read(), turnSpeed.read());
+
+	}
+
+	@Override
+	public void disabledInit() {
+		// Tell Strongback that the robot is disabled so it can flush and kill
+		// commands.
+		Strongback.disable();
+	}
+	public void switchControls() {
+		driveSpeed = driveSpeed.invert();
+		// turnSpeed = turnSpeed.invert();
+		// turnSpeed2 = turnSpeed2.invert();
+	}
 
 }
