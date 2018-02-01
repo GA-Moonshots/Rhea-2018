9955478ea81bf1cae9a6e3a3fae3f79c0574c9b1
Dan Adiletta <dan.adiletta@gmail.com>
2018-02-01 09:06:54 -0500

    Command experiments

--- src/org/frc5973/robot/JustForward.java
+++ src/org/frc5973/robot/JustForward.java
@@ -4,10 +4,10 @@ import org.strongback.Strongback;
 import org.strongback.command.Command;
 
 public class JustForward extends CommandGroup {
-    /*public JustForward() {
+    public JustForward() {
         sequentially(new CommandA(),
                      new CommandB(),
                      new CommandC());
     }
-    */
+    
 }
